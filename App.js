14c324031aecf7ff76aad9e2e66e8ea4f0369bac
lordrichard6 <paulolopesreizinho@gmail.com>
2021-05-27 20:33:23 +0200

    adding a task

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -13,14 +13,24 @@ import {
 import Task from "./components/Task";
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    // keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
 
         <View style={styles.items}>
-          <Task text="Main task" />
-          <Task text="Second task" />
+          {taskItems.map((item, index) => {
+            return <Task key={index} text={item} />;
+          })}
         </View>
       </View>
 
@@ -29,11 +39,16 @@ export default function App() {
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.writeTaskWrapper}
       >
-        <TextInput style={styles.input} placeholder={"Write a task"} />
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
-            <Text style={styles.text}>+</Text>
+            <Text style={styles.addText}>+</Text>
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
@@ -44,8 +59,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    background:
-      "linear-gradient(to bottom, #7F7FD5 0%, #86A8E7 35%, #91EAE4 100%)",
+    background: "linear-gradient(to bottom, #7F7FD5, #86A8E7, #91EAE4)",
   },
   tasksWrapper: {
     paddingTop: 80,
@@ -85,5 +99,5 @@ const styles = StyleSheet.create({
     borderColor: "#C0C0C0",
     borderWidth: 1,
   },
-  text: {},
+  addText: {},
 });
