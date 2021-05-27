ab966809ba0fcd5ba4e9c8ca24c3a03765bc8e91
lordrichard6 <paulolopesreizinho@gmail.com>
2021-05-27 14:29:34 +0200

    ockup task component

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+
+import Task from "./components/Task";
 
 export default function App() {
   return (
@@ -8,7 +10,9 @@ export default function App() {
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
 
-        <View style={styles.items}></View>
+        <View style={styles.items}>
+          <Task />
+        </View>
       </View>
     </View>
   );
